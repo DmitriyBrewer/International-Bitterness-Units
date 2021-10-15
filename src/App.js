90813ef6203387d885551c58a3e7d1e9e966068e
Dmitriy <istrabrew@mail.ru>
2021-10-15 16:37:19 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LI from "./components/LI";
 import H1 from "./components/H1";
 
 
-//GIT REACTINPUT
+//GIT
 function App() {
 
 
@@ -14,80 +14,119 @@ function App() {
   const [volumeError, setvolumeError] = useState("Введите корректный объём сусла");//State input Error состояние ошибки значения
   const [volumeClass, setvolumeClass] = useState("inPut inPut-design");//State input Classchange состояние класса input
   const [volumeh1, setvolumeH1] = useState('h1styleRed');//State valid form Состояние правильного текста h1 
-// const [volumePlaceholder, setvolumePlaceholder] = useState("Введите объем сусла, л"); //State input placeholder
 //State input Volume
 
 
-
-
-//State input Plato
+//State input Plato-SG
   const [plato, setPlato] = useState("");
   const [platoClean, setplatoClean] = useState(false); //State input Clean, состояние выхода из input
-  const [platoError, setplatoError] = useState("Введите корректную плотность сусла");//State input Error состояние ошибки значения
+  const [platoError, setplatoError] = useState("");//State input Error состояние ошибки значения
   const [platoClass, setplatoClass] = useState("inPut inPut-design PlatoSG");//State input Classchange состояние класса input
   const [platoh1, setplatoH1] = useState('h1styleRed');
-//State input Plato
+//State input Plato-SG
+
+//State input nameHops 
+const [nameHops, setnameHops] = useState(""); //State input nameHops
+const [nameHopsClean, setnameHopsClean] = useState(false); //State input Clean, состояние выхода из input
+const [nameHopsError, setnameHopsError] = useState("Введите корректное наименование");//State input Error состояние ошибки значения
+const [nameHopsClass, setnameHopsClass] = useState("inPut inPut-design");//State input Classchange состояние класса input
+const [nameHopsh1, setnameHopsH1] = useState('h1styleRed');//State valid form Состояние правильного текста h1 
+//State input nameHops
 
 
-//State input SG
-const [plato1, setPlato1] = useState("");
-const [platoClean1, setplatoClean1] = useState(false); //State input Clean, состояние выхода из input
-const [platoError1, setplatoError1] = useState("Введите корректную плотность сусла");//State input Error состояние ошибки значения
-const [platoClass1, setplatoClass1] = useState("inPut inPut-design PlatoSG");//State input Classchange состояние класса input
-const [platoh11, setplatoH11] = useState('h1styleRed');
-//State input SG
+//State input timeHops 
+const [timeHops, settimeHops] = useState(""); //State input timeHops
+const [timeHopsClean, settimeHopsClean] = useState(false); //State input Clean, состояние выхода из input
+const [timeHopsError, settimeHopsError] = useState("Введите корректный объём сусла");//State input Error состояние ошибки значения
+const [timeHopsClass, settimeHopsClass] = useState("inPut inPut-design");//State input Classchange состояние класса input
+const [timeHopsh1, settimeHopsH1] = useState('h1styleRed');//State valid 
+//State input timeHops
 
 
+//State input boil 
+const [boil, setboil] = useState(""); //State input boil
+const [boilClean, setboilClean] = useState(false); //State input Clean, состояние выхода из input
+const [boilError, setboilError] = useState("Введите корректный объём сусла");//State input Error состояние ошибки значения
+const [boilClass, setboilClass] = useState("inPut inPut-design");//State input Classchange состояние класса input
+const [boilh1, setboilH1] = useState('h1styleRed');//State valid 
+//State input boil
+
+
+//State - Button Og, Plato
+const [butPlato, newClassPlato] = useState('butPurp');
+const [butOG, newClassOG] = useState('but');
+const [span, setSpan] = useState('');// это Plato
+const [span2, setSpan2] = useState('inPut-hidden');// это SG
+const [placeholderSG, setplaceholerSG] = useState('Введите плотность SG');
+//State - Button Og, Plato
 
 
 //State IBU
 const [ibu, setIbu] = useState("");
-
 const [classSubmit, setclassSubmit] = useState("inPut buttongrey")
 const [validForm, setFormValid] = useState(false); // разблокирова кнопки Посчитать IBU
+//State IBU
 
-//Поработать над useEffect условие if не совсем верно выполняется
+
+
+//!! Submit
 useEffect(() => {
   if (volumeError || platoError){
     setFormValid(true);
     setclassSubmit("inPut button1");
   } else setFormValid(false);
 }, [volumeError, platoError]);
+
+//Handle Submit IBU
+function handleSubmit(e){
+  e.preventDefault();
+  if(!volume.trim() || !plato.trim() || !boil.trim() || !timeHops.trim()){
+    setIbu('Введите поля корректно');
+  } else 
+  setIbu('volume= ' + volume + ' plato =  ' + plato);
+};
+//Handle Submit IBU
 // чтобы установить форму нужно на button который отвечает за  Submit повесить disabled={!validForm};
-
-///State IBU
-
+// Submit
 
 
-//State - Button Og, Plato
-  const [butPlato, newClassPlato] = useState('butPurp');
-  const [butOG, newClassOG] = useState('but');
-  const [span, setSpan] = useState('');// это Plato
-  const [span2, setSpan2] = useState('inPut-hidden');// это SG
+
+
+
 
   // Submit PLATO OG
-  function SubmitPlato(e) {
-    e.preventDefault();
-    // e.className = "butPurp"
-    newClassPlato('butPurp');
-    newClassOG('but');
-    setSpan(''); //при нажатии пропадает input
-    setSpan2('inPut-hidden');
-    var Plato = 'Plato'
-    return console.log(Plato);
-  };
+  function Submit(e){
+    switch (e.target.name){
+      case 'PLATO':
+        e.preventDefault();
+        newClassPlato('butPurp');
+        newClassOG('but');
+        setSpan(''); //при нажатии пропадает input
+        setSpan2('inPut-hidden'); 
+        setPlato(plato)// сюда встает обратная мат.функция changeSG
+    break;
 
-  function SubmitOG(e) {
-    e.preventDefault();
-    newClassPlato('but');
-    newClassOG('butPurp');
-    setSpan('inPut-hidden'); //при нажатии пропадает input
-    setSpan2('')
-    var SG = 'SG'
-    return console.log(SG);
-  };
+    case 'SG':
+      e.preventDefault();
+      newClassPlato('but');
+      newClassOG('butPurp');
+      setSpan('inPut-hidden'); //при нажатии пропадает input
+      setSpan2('')
+      setplatoH1('h1styleHidden');//скрываем текст ошибки дл инпут
+      //функция changeSG переводит Plato в SG и выдает это значение не в value, а в placeholder 
+      function changeSG(pl){
+        if (!pl.trim() || plato>=36 || plato<=5) {return "Введите плотност SG"
+      } else
+        var sg = (1 + (pl / (258.6 - (((pl) / 258.2) * 227.1))));
+        var parsSG = parseFloat(sg.toFixed(2));
+        return parsSG;
+      }
+      setplaceholerSG(changeSG(plato));
+      setPlato(e.target.value);
+      break;
+      default:
+  }};
 // Submit PLATO OG
-//State - Button Og, Plato
 
 
 
@@ -96,7 +135,8 @@ function blurHandler(e){
   switch (e.target.name){
     case 'volume':
       setvolumeClean(true);
-        if (!volume.trim() || volume > 100000){
+      //Как сделать чтобы выражение g12g213 тоже не валидировалась. || !volume.replace(/[^\d.]/g, '') || !volume.replace(/[^+\d]/g, '') || !volume.replace(/[a-zA-Z]/) || volume.replace(/[a-zA-Zа-яА-Я]/)
+        if (!volume.trim() || volume <= 0.00001 || volume >= 100000 || !volume.replace(/[^\d.]/g, '') || !volume.replace(/[^+\d]/g, '')){
           setvolumeError('Error Некорректное значение объёма');//Описываем состояние ошибки
           setvolumeClass('inPut inPut-design inPut-danger');//Описываем состояние ошибки
           setvolumeH1('h1styleRed');
@@ -105,21 +145,50 @@ function blurHandler(e){
 
     case 'plato':
       setplatoClean(true);
-      if (!plato.trim() || plato > 36){
+      if (!plato.trim() || plato>=36 || plato<=5){
         setplatoError('Error Некорректное значение плотности');//Описываем состояние ошибки
         setplatoClass('inPut inPut-design PlatoSG inPut-danger');//Описываем состояние ошибки
         setplatoH1('h1styleRed');
 };
       break;
 
-      case 'plato1':
-      setplatoClean1(true);
-      if (!plato1.trim() || plato1 > 1.1){
-        setplatoError1('Error Некорректное значение плотности');//Описываем состояние ошибки
-        setplatoClass1('inPut inPut-design PlatoSG inPut-danger');//Описываем состояние ошибки
-        setplatoH11('h1styleRed');
-};
-      break;
+      case 'sg':
+        setplatoClean(true);
+        if (!plato.trim() || plato>=1.09 || plato<=1){
+          setplatoError('ErrAr Некорректное значение плотности');//Описываем состояние ошибки
+          setplatoClass('inPut inPut-design PlatoSG inPut-danger');//Описываем состояние ошибки
+          setplatoH1('h1styleRed');
+  };
+        break;
+
+      case 'nameHops':
+        setnameHopsClean(true);
+        if (!nameHops.trim()){
+          setnameHopsError('Error Некорректное наименование');//Описываем состояние ошибки
+          setnameHopsClass('inPut inPut-design inPut-danger');//Описываем состояние ошибки
+          setnameHopsH1('h1styleRed');
+  };
+    break;
+
+      case 'boil':
+        setboilClean(true);
+          if (!boil.trim() || boil <= 0.00001 || boil >= 201 || !boil.replace(/[^\d.]/g, '') || !boil.replace(/[^+\d]/g, '')){
+            setboilError('Error Некорректное значение объёма');//Описываем состояние ошибки
+            setboilClass('inPut inPut-design inPut-danger');//Описываем состояние ошибки
+            setboilH1('h1styleRed');
+  };
+    break;
+
+      case 'timeHops':
+        settimeHopsClean(true);
+          if (!timeHops.trim() || timeHops <= 0.00001 || timeHops >= 100000 || !timeHops.replace(/[^\d.]/g, '') || !timeHops.replace(/[^+\d]/g, '')){
+            settimeHopsError('Error Некорректное значение объёма');//Описываем состояние ошибки
+            settimeHopsClass('inPut inPut-design inPut-danger');//Описываем состояние ошибки
+            settimeHopsH1('h1styleRed');
+  };
+    break;
+
+            
       default://иначе выдает Expected a default case default-case
   };
 };
@@ -128,19 +197,11 @@ function blurHandler(e){
 
 
 
-
 //Handlers. Ослеживание состояния ввода value для input
 const volumeHandler = (e) => {
   setVolume(e.target.value);
   console.log(e.target.value);
-    if (!volume.trim()){
-      //эту часть можно удалить
-      console.log('корректно');
-      setvolumeError('Некорректное значение');
-      setvolumeClass('inPut inPut-design inPut-danger');
-      //эту часть можно удалить
-      
-    } else 
+    if (!volume.trim()){} else 
     console.log('корректное');
     setvolumeH1('h1styleTrue');
     setvolumeError('Корректное значение');//Описываем состояние правильного ввода
@@ -148,68 +209,48 @@ const volumeHandler = (e) => {
 };
 
 const platoHandler = (e) => {
-  setPlato(e.target.value); 
+  // switch (e.target.name) {}
+  setPlato(e.target.value);
+  if (!plato.trim()){} else 
+    console.log('корректное');
+    setplatoH1('h1styleTrue');
+    setplatoError('Корректное значение');//Описываем состояние правильного ввода
+    setplatoClass('inPut inPut-design PlatoSG inPut-true');//Описываем состояние правильного ввода
+}// сделать корректное состояние
+
+const nameHopsHandler = (e) => {
+  setnameHops(e.target.value);
   console.log(e.target.value);
-  if (!plato.trim()){
-    //эту часть можно удалить
-    console.log('корректно');
-    setplatoError('Некорректное значение');
-    setplatoClass('inPut inPut-design PlatoSG inPut-danger');
-    //эту часть можно удалить
-    
-  } else 
-  console.log('корректное');
-  setplatoH1('h1styleTrue');
-  setplatoError('Корректное значение');//Описываем состояние правильного ввода
-  setplatoClass('inPut inPut-design PlatoSG inPut-true');//Описываем состояние правильного ввода
+    if (!nameHops.trim()){} else 
+    console.log('корректное');
+    setnameHopsH1('h1styleTrue');
+    setnameHopsError('Корректное значение');//Описываем состояние правильного ввода
+    setnameHopsClass('inPut inPut-design inPut-true');//Описываем состояние правильного ввода
 };
 
-const platoHandler1 = (e) => {
-  setPlato1(e.target.value); 
+const boilHandler = (e) => {
+  setboil(e.target.value);
   console.log(e.target.value);
-  if (!plato1.trim()){
-    //эту часть можно удалить
-    console.log('корректно');
-    setplatoError1('Некорректное значение');
-    setplatoClass1('inPut inPut-design PlatoSG inPut-danger');
-    //эту часть можно удалить
-    
-  } else 
-  console.log('корректное');
-  setplatoH11('h1styleTrue');
-  setplatoError1('Корректное значение');//Описываем состояние правильного ввода
-  setplatoClass1('inPut inPut-design PlatoSG inPut-true');//Описываем состояние правильного ввода
+    if (!boil.trim()){} else 
+    console.log('корректное');
+    setboilH1('h1styleTrue');
+    setboilError('Корректное значение');
+    setboilClass('inPut inPut-design inPut-true');
 };
 
-
+const timeHopsHandler = (e) => {
+  settimeHops(e.target.value);
+  console.log(e.target.value);
+    if (!timeHops.trim()){} else 
+    console.log('корректное');
+    settimeHopsH1('h1styleTrue');
+    settimeHopsError('Корректное значение');
+    settimeHopsClass('inPut inPut-design inPut-true');
+};
 
 function ibuHandler () {};//разблокировка изменеия input
 //Handlers
 
-
-
-
-
-
-
-
-
-
-//Handle Submit IBU
-    function handleSubmit(e){
-      e.preventDefault();
-      if(!volume.trim() || !plato.trim()){
-        setIbu('Введите поля корректно');
-      } else 
-      setIbu('volume= ' + volume + ' plato =  ' + plato);
-    };
-//Handle Submit IBU
-
-
-
-
-
-//
   return (
   <div>
     <header className="navbarStyle">
@@ -221,18 +262,27 @@ function ibuHandler () {};//разблокировка изменеия input
             <ul className="UL" id="ulFRAME">
               <form onSubmit={handleSubmit}>
                 <LI/><H1 className="h1styleBlack" textContent="Введите параметры сусла"/>
-                <LI/><input name="volume" placeholder='Введите объем сусла, л' className={volumeClass} type="text" value={volume} onChange={e=>volumeHandler(e)} onBlur={blurHandler}/>
-
+                <LI/><input name="volume" placeholder='Введите объем сусла, л' className={volumeClass} type="number" value={volume} onChange={e=>volumeHandler(e)} onBlur={blurHandler}/>
                 {(volumeClean && volumeError) && <h1 className={volumeh1} >{volumeError}</h1>}
 
-                <span className={span}><LI/><input name="plato" placeholder="Введите плотность сусла" className={platoClass} type="text" value={plato} onChange={e=>platoHandler(e)} onBlur={blurHandler}/><button className={butPlato} onClick={SubmitPlato} type="radio">Plato</button>
-                  <button className={butOG} onClick={SubmitOG} type="radio">SG</button></span>
+                <span className={span}><LI/><input name="plato" placeholder="Введите плотность сусла" className={platoClass} type="number" value={plato} onChange={e=>platoHandler(e)} onBlur={blurHandler}/></span>
+                <span className={span2}><LI/><input name="sg" placeholder={placeholderSG} className={platoClass} type="number" value={plato} onChange={e=>platoHandler(e)} onBlur={blurHandler}/></span>
+                  <button name='PLATO' className={butPlato} onClick={Submit} type="radio">Plato</button>
+                  <button name='SG' className={butOG} onClick={Submit} type="radio">SG</button>
                   {(platoClean && platoError) && <h1 className={platoh1} >{platoError}</h1>}
 
-                <span className={span2}><LI/><input name="plato1" placeholder="Введите плотность SG" className={platoClass1} type="text" value={plato1} onChange={e=>platoHandler1(e)} onBlur={blurHandler}/><button className={butPlato} onClick={SubmitPlato} type="radio">Plato</button>
-                  <button className={butOG} onClick={SubmitOG} type="radio">SG</button></span>
+                  <LI/><input name="nameHops" placeholder='Наименование хмеля' className={nameHopsClass} type="text" value={nameHops} onChange={e=>nameHopsHandler(e)} onBlur={blurHandler}/>
+                {(nameHopsClean && nameHopsError) && <h1 className={nameHopsh1} >{nameHopsError}</h1>}
 
-                {(platoClean1 && platoError1) && <h1 className={platoh11} >{platoError1}</h1>}
+                <LI/><input name="boil" placeholder='Кипячение, мин' className={boilClass} type="number" value={boil} onChange={e=>boilHandler(e)} onBlur={blurHandler}/>
+                {(boilClean && boilError) && <h1 className={boilh1} >{boilError}</h1>}
+
+                <LI/><input name="timeHops" placeholder='Время внесения' className={timeHopsClass} type="number" value={timeHops} onChange={e=>timeHopsHandler(e)} onBlur={blurHandler}/>
+                {(timeHopsClean && timeHopsError) && <h1 className={timeHopsh1} >{timeHopsError}</h1>}
+
+
+                
+
                 <LI/><button className={classSubmit} id="buttonNEXT" type="submit" disabled={!validForm}>Посчитать IBU</button>  
               </form>
               <LI/><input placeholder="IBU" className="inPut inPut-design" value={ibu} onChange={ibuHandler} />
