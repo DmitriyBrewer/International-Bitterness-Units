c0f02dd8214aad6161c87d168911fb0f6205c728
Dmitriy <istrabrew@mail.ru>
2021-10-16 12:53:02 +0300

    h

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ const [placeholderSG, setplaceholerSG] = useState('Введите плотнос
 
 //State IBU
 const [ibu, setIbu] = useState("");
+const [ibuSubmit, setibuSubmit] = useState(false);
 const [classSubmit, setclassSubmit] = useState("inPut buttongrey")
 const [validForm, setFormValid] = useState(false); // разблокирова кнопки Посчитать IBU
 //State IBU
@@ -96,6 +97,7 @@ useEffect(() => {
 //Handle Submit IBU
 function handleSubmit(e){
   e.preventDefault();
+  
   if(!volume.trim() || !plato.trim() || !boil.trim() || !timeHops.trim()){
     setIbu('Введите поля корректно');
   } else 
@@ -309,22 +311,123 @@ function ibuHandler () {};//разблокировка изменеия input
 //Handlers
 
 
+// Calculate 
+// IBU calс 
+function IBU(){
+  var ibu = ((amount * alpha) / (volume * 0.1))*(KOEF(OGSG(plato), (boil - timeHops)));
+  var parsIBU = parseFloat(ibu.toFixed(1));
+  return parsIBU
+};
 
-function Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops){
-  // if (plato >= 1.09){
-  //   var sg = (1 + (plato / (258.6 - (((plato) / 258.2) * 227.1))));
-  //       var parsSG = parseFloat(sg.toFixed(2));
-  //       return parsSG
-  // } //рабочее выражение
-  var object ={
-  // og : [1.030, 1.040, 1.050, 1.060, 1.070],
-  // boil : [5, 10, 15, 20, 30, 40, 60, 80, 90, 120],
-  // numstring : ["00", 10, 20, 30, 40, "01", 11, 21, 31, 41, "02", 12, 22, 32, 42, "03", 13, 23, 33, 34, "04", 14, 24, 34, 44, "05", 15, 25, 35, 45, "06", 16, 26, 36, 46, "07", 17, 27, 37, 47, "08", 18, 28, 38, 48, "09", 19, 29, 39, 49],
-  // string1 : [0.055, 0.050, 0.045, 0.042, 0.038,0.1, 0.091, 0.084, 0.076, 0.070, 0.137, 0.125, 0.114, 0.105, 0.096, 0.167, 0.153, 0.14, 0.128, 0.117, 0.212, 0.194, 0.177, 0.162, 0.148, 0.242, 0.221, 0.202, 0.185, 0.169, 0.276, 0.252, 0.231, 0.211, 0.193, 0.291, 0.266, 0.243, 0.222, 0.203, 0.295, 0.27, 0.247, 0.226, 0.206, 0.301, 0.275, 0.252, 0.23, 0.21]
-}
- var ibu = ((amount * alpha) / (volume * 0.1)) * 0.21
-  return ibu
-}
+
+//  IBU calс
+//OGSG Функция пересчёта OG в SG
+function OGSG(plato) {
+  if (plato >= 1.09){
+        var sg = (1 + (plato / (258.6 - (((plato) / 258.2) * 227.1))));
+            var parsSG = parseFloat(sg.toFixed(2));
+            return parsSG
+      } 
+      else return plato
+};
+//OGSG Функция пересчёта OG в SG
+
+
+
+//KOEF функция расчета коэффицента изомеризации
+function KOEF (plato, timeHops) {
+  var koef
+  var KOEF_1 = [0.055, 0.1, 0.137, 0.167, 0.212, 0.242, 0.276, 0.291, 0.295, 0.301];
+  if (plato <= 1.0301) {
+      if (timeHops <=5.1){koef = KOEF_1[0]};
+      if (timeHops <= 10.1 && timeHops >=5.1){koef = KOEF_1[1]};
+      if (timeHops <= 15.1 && timeHops >=10.1){koef = KOEF_1[2]};
+      if (timeHops <= 20.1 && timeHops >=15.1){koef = KOEF_1[3]};
+      if (timeHops <= 30.1 && timeHops >=20.1){koef = KOEF_1[4]};
+      if (timeHops <= 40.1 && timeHops >=30.1){koef = KOEF_1[5]};
+      if (timeHops <= 60.1 && timeHops >=40.1){koef = KOEF_1[6]};
+      if (timeHops <= 80.1 && timeHops >=60.1){koef = KOEF_1[7]};
+      if (timeHops <= 90.1 && timeHops >=80.1){koef = KOEF_1[8]};
+      if (timeHops <= 120.1 && timeHops >=90.1){koef = KOEF_1[9]};
+      if (timeHops <= 300.1 && timeHops >=120.1){koef = KOEF_1[9]};
+      console.log('Итого koef = ' + koef);
+      return koef
+  };
+
+  var KOEF_2 = [0.05, 0.091, 0.125, 0.153, 0.194, 0.221, 0.252, 0.266, 0.27, 0.275];
+  if (plato <= 1.0401) {
+      if (timeHops <=5.1){koef = KOEF_2[0]};
+      if (timeHops <= 10.1 && timeHops >=5.1){koef = KOEF_2[1]};
+      if (timeHops <= 15.1 && timeHops >=10.1){koef = KOEF_2[2]};
+      if (timeHops <= 20.1 && timeHops >=15.1){koef = KOEF_2[3]};
+      if (timeHops <= 30.1 && timeHops >=20.1){koef = KOEF_2[4]};
+      if (timeHops <= 40.1 && timeHops >=30.1){koef = KOEF_2[5]};
+      if (timeHops <= 60.1 && timeHops >=40.1){koef = KOEF_2[6]};
+      if (timeHops <= 80.1 && timeHops >=60.1){koef = KOEF_2[7]};
+      if (timeHops <= 90.1 && timeHops >=80.1){koef = KOEF_2[8]};
+      if (timeHops <= 120.1 && timeHops >=90.1){koef = KOEF_2[9]};
+      if (timeHops <= 300.1 && timeHops >=120.1){koef = KOEF_2[9]};
+      console.log('Итого koef = ' + koef);
+      return koef
+  };
+
+  var KOEF_3 = [0.046, 0.084, 0.114, 0.14, 0.177, 0.202, 0.231, 0.243, 0.247, 0.252];
+  if (plato <= 1.0501) {
+      if (timeHops <=5.1){koef = KOEF_3[0]};
+      if (timeHops <= 10.1 && timeHops >=5.1){koef = KOEF_3[1]};
+      if (timeHops <= 15.1 && timeHops >=10.1){koef = KOEF_3[2]};
+      if (timeHops <= 20.1 && timeHops >=15.1){koef = KOEF_3[3]};
+      if (timeHops <= 30.1 && timeHops >=20.1){koef = KOEF_3[4]};
+      if (timeHops <= 40.1 && timeHops >=30.1){koef = KOEF_3[5]};
+      if (timeHops <= 60.1 && timeHops >=40.1){koef = KOEF_3[6]};
+      if (timeHops <= 80.1 && timeHops >=60.1){koef = KOEF_3[7]};
+      if (timeHops <= 90.1 && timeHops >=80.1){koef = KOEF_3[8]};
+      if (timeHops <= 120.1 && timeHops >=90.1){koef = KOEF_3[9]};
+      if (timeHops <= 300.1 && timeHops >=120.1){koef = KOEF_3[9]};
+      console.log('Итого koef = ' + koef);
+      return koef
+  };
+
+  var KOEF_4 = [0.042, 0.076, 0.105, 0.128, 0.162, 0.185, 0.211, 0.222, 0.226, 0.23];
+  if (plato <= 1.0601) {
+      if (timeHops <=5.1){koef = KOEF_4[0]};
+      if (timeHops <= 10.1 && timeHops >=5.1){koef = KOEF_4[1]};
+      if (timeHops <= 15.1 && timeHops >=10.1){koef = KOEF_4[2]};
+      if (timeHops <= 20.1 && timeHops >=15.1){koef = KOEF_4[3]};
+      if (timeHops <= 30.1 && timeHops >=20.1){koef = KOEF_4[4]};
+      if (timeHops <= 40.1 && timeHops >=30.1){koef = KOEF_4[5]};
+      if (timeHops <= 60.1 && timeHops >=40.1){koef = KOEF_4[6]};
+      if (timeHops <= 80.1 && timeHops >=60.1){koef = KOEF_4[7]};
+      if (timeHops <= 90.1 && timeHops >=80.1){koef = KOEF_4[8]};
+      if (timeHops <= 120.1 && timeHops >=90.1){koef = KOEF_4[9]};
+      if (timeHops <= 300.1 && timeHops >=120.1){koef = KOEF_4[9]};
+      console.log('Итого koef = ' + koef);
+      return koef
+  };
+
+  var KOEF_5 = [0.038, 0.070, 0.096, 0.117, 0.148, 0.169, 0.193, 0.203, 0.206, 0.21];
+  if (plato <= 1.0701 || plato >= 1.0701) {
+      if (timeHops <=5.1){koef = KOEF_5[0]};
+      if (timeHops <= 10.1 && timeHops >=5.1){koef = KOEF_5[1]};
+      if (timeHops <= 15.1 && timeHops >=10.1){koef = KOEF_5[2]};
+      if (timeHops <= 20.1 && timeHops >=15.1){koef = KOEF_5[3]};
+      if (timeHops <= 30.1 && timeHops >=20.1){koef = KOEF_5[4]};
+      if (timeHops <= 40.1 && timeHops >=30.1){koef = KOEF_5[5]};
+      if (timeHops <= 60.1 && timeHops >=40.1){koef = KOEF_5[6]};
+      if (timeHops <= 80.1 && timeHops >=60.1){koef = KOEF_5[7]};
+      if (timeHops <= 90.1 && timeHops >=80.1){koef = KOEF_5[8]};
+      if (timeHops <= 120.1 && timeHops >=90.1){koef = KOEF_5[9]};
+      if (timeHops <= 300.1 && timeHops >=120.1){koef = KOEF_5[9]};
+      console.log('Итого koef = ' + koef);
+      return koef
+  };
+};
+//KOEF функция расчета коэффицента изомеризации
+
+//  var ibu = ((amount * alpha) / (volume * 0.1)) * 0.21
+//   return ibu
+// }
+//
 
   return (
   <div>
@@ -349,7 +452,7 @@ function Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops){
                   <LI/><input name="nameHops" placeholder='Наименование хмеля' className={nameHopsClass} type="text" value={nameHops} onChange={e=>nameHopsHandler(e)} onBlur={blurHandler}/>
                 {(nameHopsClean && nameHopsError) && <h1 className={nameHopsh1} >{nameHopsError}</h1>}
 
-                <LI/><input name="boil" placeholder='Кипячение, мин' className={boilClass} type="number" value={boil} onChange={e=>boilHandler(e)} onBlur={blurHandler}/>
+                <LI/><input name="boil" placeholder='Кипячение сусла, мин' className={boilClass} type="number" value={boil} onChange={e=>boilHandler(e)} onBlur={blurHandler}/>
                 {(boilClean && boilError) && <h1 className={boilh1} >{boilError}</h1>}
 
                 <LI/><input name="alpha" placeholder='Альфа кислота, %' className={alphaClass} type="number" value={alpha} onChange={e=>alphaHandler(e)} onBlur={blurHandler}/>
@@ -359,7 +462,7 @@ function Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops){
                 {(amountClean && amountError) && <h1 className={amounth1} >{amountError}</h1>}
 
 
-                <LI/><input name="timeHops" placeholder='Время внесения' className={timeHopsClass} type="number" value={timeHops} onChange={e=>timeHopsHandler(e)} onBlur={blurHandler}/>
+                <LI/><input name="timeHops" placeholder='Время внесения хмеля, мин' className={timeHopsClass} type="number" value={timeHops} onChange={e=>timeHopsHandler(e)} onBlur={blurHandler}/>
                 {(timeHopsClean && timeHopsError) && <h1 className={timeHopsh1} >{timeHopsError}</h1>}
 
 
@@ -367,7 +470,7 @@ function Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops){
 
                 <LI/><button className={classSubmit} id="buttonNEXT" type="submit" disabled={!validForm}>Посчитать IBU</button>  
               </form>
-              <LI/><input placeholder="IBU" className="inPut inPut-design" value={Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops)} onChange={ibuHandler} />
+              <LI/><input placeholder="IBU" className="inPut inPut-design" value={ibuSubmit && IBU()}/>
             </ul>
         </div>
     </div>
@@ -376,3 +479,6 @@ function Calculate(volume, plato, nameHops, boil, alpha, amount, timeHops){
     
 
 export default App;
+
+
+//  var ibu = ((amount * alpha) / (volume * 0.1)) * 0.21
