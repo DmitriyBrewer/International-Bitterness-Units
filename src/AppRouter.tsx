7d8ceae2fa6c1677e0a1c65a4dd15b68fe15e654
Dmitriy <istrabrew@mail.ru>
2022-05-28 18:14:01 +0300

    other

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -7,16 +7,12 @@ import About from "./pages/About";
 import CalcTestVersion from "./pages/TestCalc/CalcTestVersion";
 
 
-
-
-
-
 function AppRouter() {
   return (
     <Routes>
       <Route path="/calc" element={<CalcTestVersion />} />
-      {/* <Route path="/login" element={<Login />}/> */}
-      {/* <Route path="/main" element={<Main />}/> */}
+      <Route path="/login" element={<Login />}/>
+      <Route path="/main" element={<Main />}/>
       <Route path="/about" element={<About />}/>
       {/* <Route path="/test" element={<CalcTestVersion />}/> */}
 
